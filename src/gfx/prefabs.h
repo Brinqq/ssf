0ee5a96c56f8a3c2fb058366c9fd967008d67137
brinq <andrewrdavis18@gmail.com>
2025-11-07 12:31:31 -0800

    format i guess

--- src/gfx/prefabs.h
+++ src/gfx/prefabs.h
@@ -6,7 +6,6 @@
 namespace ssf{
   namespace prefabs{
 
-
   template<typename _IndexType>
   struct StandardCube{
     static constexpr float vertices[24] = {
@@ -21,7 +20,7 @@ namespace ssf{
         -0.5f,  0.5f, -0.5f
     };
 
-    static constexpr uint16_t indices[36] = {
+    static constexpr _IndexType indices[36] = {
         0, 1, 2,  2, 3, 0,
         5, 4, 7,  7, 6, 5,
         3, 2, 6,  6, 7, 3,
