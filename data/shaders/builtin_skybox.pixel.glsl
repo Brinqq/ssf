87874e84afb972af860097bb1f2112f6651e0365
brinq <andrewrdavis18@gmail.com>
2025-12-14 20:26:26 -0800

    basic background for now

--- data/shaders/builtin_skybox.pixel.glsl
+++ data/shaders/builtin_skybox.pixel.glsl
@@ -1,9 +1,13 @@
 #version 450
 
+layout(location = 0) in vec2 texcoord;
+
 layout(location = 0) out vec4 pixel;
 
 layout(binding = 0) uniform sampler2D tex;
 
+vec4 skybox_color = vec4(0.4f, 0.5f, 0.6f, 1.0f);
+
 void main(){
-  pixel = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+  pixel = skybox_color;
 }
--- data/shaders/builtin_skybox.vert.glsl
+++ data/shaders/builtin_skybox.vert.glsl
@@ -1,5 +1,14 @@
 #version 450
 
+layout(location = 0)out vec2 texcoord;
+
+vec2 vertices[3] = vec2[3](
+  vec2(-1.0, -1.0),
+  vec2( 3.0, -1.0),
+  vec2(-1.0,  3.0)
+);
+
 void main(){
-  
+    gl_Position = vec4(vertices[gl_VertexIndex], 0.99f, 1.0);
+    texcoord = vertices[gl_VertexIndex] * 0.5 + 0.5;
 }
