e8ca837e677f2efb221425b220235ce8e42e5944
brinq <andrewrdavis18@gmail.com>
2025-12-15 04:13:37 -0800

    env setup script

---
+++ scripts/env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DYLD_LIBRARY_PATH  = "/Users/brinq/.dev/librarys/VulkanSDK/1.4.328.1/macOS/lib"
+export VK_ICD_FILENAMES   = "/Users/brinq/.dev/librarys/VulkanSDK/1.4.328.1/macOS/share/vulkan/icd.d/MoltenVK_icd.json"
+export VK_LAYER_PATH      = "/Users/brinq/.dev/librarys/VulkanSDK/1.4.328.1/macOS/share/vulkan/explicit_layer.d"
+export SSF_VULKAN_BIN     = "/Users/brinq/.dev/librarys/VulkanSDK/1.4.328.1/macOS/bin"
