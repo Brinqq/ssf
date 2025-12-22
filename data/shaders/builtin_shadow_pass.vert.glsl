55f6af1964110f0d25ca570f7dbcd0ba8d88b922
brinq <andrewrdavis18@gmail.com>
2025-12-21 22:42:39 -0800

    shadow map

--- data/shaders/builtin_shadow_pass.vert.glsl
+++ data/shaders/builtin_shadow_pass.vert.glsl
@@ -1,5 +1,14 @@
 #version 450
 
+layout (location = 0) in vec3 vertex;
+layout (location = 1) in vec2 uv;
+layout (location = 2) in vec3 normal;
+
+layout(push_constant) uniform matrix{
+  mat4 model;
+  mat4 projection;
+};
+
 void main(){
-  
+  gl_Position = projection * model * vec4(vertex, 1.0f);
 }
